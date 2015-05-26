60b8aef42aba4e2cd7083a78266810a01bb5d45c
comSaaxov <comradesaaxov@gmail.com>
2015-05-26 13:48:40 +0300

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		concat:{
+
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.registerTasks('default',['concat']);
+};
